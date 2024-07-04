96272ef257c62906eee1d72d928c765022ca8da5
jauntier <nderituadrian0@gmail.com>
2024-07-05 02:59:28 +0300

    Fixed syntax error in settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -151,4 +151,4 @@ if not DEBUG:
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoFiel
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
